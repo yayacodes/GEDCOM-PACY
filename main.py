b7f1f8278f735252c1a421c3af8c99905d7a6d91
Charles Bethin <charlesbethin@Charless-MacBook-Pro.local>
2020-02-19 19:01:06 -0500

    move main file to root

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from gedcom.GedcomParser import GedcomParser
 
 if __name__ == '__main__':
     print('MAIN CODE HERE')
-    gedcom = GedcomParser().parse('test_family.ged')
+    gedcom = GedcomParser().parse('res/test_family.ged')
     gedcom.pretty_print()
 
     problems = validate_gedcom(gedcom)
