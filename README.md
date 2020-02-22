4ecfecfb81aa68cab4c9d377abf29e1819d97b96
prothepsmesh <protogdyt@gmail.com>
2020-02-22 17:11:33 -0500

    reboot

--- README.md
+++
@@ -1,5 +0,0 @@
-# GEDCOM-PACY
-CS/SSW 555 Group Project Repo
-
-TODO:
-- Meet @ 9:15 PM EST 
--- gedcom/validation.py
+++
@@ -1,9 +0,0 @@
-# Validation methods go here
-
-all_validators = []
-
-
-def validate_gedcom(gedcom):
-    errors = []
-    for validator in all_validators:
-        errors.extend(validator(gedcom))
