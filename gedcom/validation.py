1dfe796d42e2e15ea0a4cc5317ac6c979294352e
Ayman Elkfrawy <elkfrawy@gmail.com>
2020-02-22 17:56:03 -0800

    Implement divorce before death validation

--- gedcom/validation.py
+++ gedcom/validation.py
@@ -9,7 +9,45 @@ def validate_too_old_individual(gedcom):
     return result
 
 
-all_validators = [validate_too_old_individual]
+def validate_marriage_before_death(gedcom):
+    """
+    Marriage should occur before death of either spouse
+    """
+    result = []
+    for family in gedcom.families:
+        marriage_date = family.married
+        if marriage_date:
+            husband = gedcom.individual_with_id(family.husband_id)
+            wife = gedcom.individual_with_id(family.wife_id)
+            if husband.death < marriage_date:
+                result.append(f'Error: Family ({family.id}) has the husband {husband.name} ({family.husband_id})'
+                              f' death date ({husband.death.strftime("%x")}) before the marriage date ({marriage_date.strftime("%x")})')
+            if wife.death < marriage_date:
+                result.append(f'Error: Family ({family.id}) has the wife {wife.name} ({family.wife_id})'
+                              f' death date ({wife.death.strftime("%x")}) before the marriage date ({marriage_date.strftime("%x")})')
+    return result
+
+
+def validate_divorce_before_death(gedcom):
+    """
+    Divorce can only occur before death of both spouses
+    """
+    result = []
+    for family in gedcom.families:
+        divorce_date = family.divorced
+        if divorce_date:
+            husband = gedcom.individual_with_id(family.husband_id)
+            wife = gedcom.individual_with_id(family.wife_id)
+            if husband.death < divorce_date:
+                result.append(f'Error: Family ({family.id}) has the husband {husband.name} ({family.husband_id})'
+                              f' death date ({husband.death.strftime("%x")}) before the divorce date ({divorce_date.strftime("%x")})')
+            if wife.death < divorce_date:
+                result.append(f'Error: Family ({family.id}) has the wife {wife.name} ({family.wife_id})'
+                              f' death date ({wife.death.strftime("%x")}) before the divorce date ({divorce_date.strftime("%x")})')
+    return result
+
+
+all_validators = [validate_too_old_individual, validate_marriage_before_death, validate_divorce_before_death]
 
 
 def validate_gedcom(gedcom):
--- tests/test_validations.py
+++ tests/test_validations.py
@@ -1,11 +1,32 @@
-from gedcom.validation import validate_too_old_individual
+from gedcom.validation import *
 from gedcom.GedcomParser import GedcomParser
 
 
-def test_validate_too_old_individual():
+def parse_gedcom(file):
     parser = GedcomParser()
-    gedcom = parser.parse('../res/family_validation.ged')
+    gedcom = parser.parse(file)
+    return gedcom
+
+def test_validate_too_old_individual():
+    gedcom = parse_gedcom('../res/family_validation.ged')
     errors = validate_too_old_individual(gedcom)
     assert len(errors) == 1
     assert errors[0] == 'Error: The individual Morgan Freeman (I01) is too old, age = 151.'
+
+
+def test_marriage_before_death():
+    gedcom = parse_gedcom('../res/marriage_before_death_test.ged')
+    errors = validate_marriage_before_death(gedcom)
+    assert len(errors) == 1
+    assert errors[0] == 'Error: Family (F01) has the husband Morgan Freeman (I01) death date (01/02/00) before the marriage date (01/01/01)'
+
+
+def test_divorce_before_death():
+    gedcom = parse_gedcom('../res/divorce_before_death_test.ged')
+    errors = validate_divorce_before_death(gedcom)
+    assert len(errors) == 1
+    assert errors[0] == 'Error: Family (F01) has the husband Morgan Freeman (I01) death date (01/02/00) before the divorce date (01/01/01)'
+
+
+
 
