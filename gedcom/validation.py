35958412be07bd6995ac495894b64d1765ec5c2b
Charles Bethin <charlesbethin@Charless-MacBook-Pro.local>
2020-02-23 20:48:01 -0500

    Add more information to tests

--- gedcom/validation.py
+++ gedcom/validation.py
@@ -5,7 +5,7 @@ def validate_too_old_individual(gedcom):
     result = []
     for individual in gedcom.individuals:
         if individual.age > 150:
-            result.append(f'Error: The individual {individual.name} ({individual.id}) is too old, age = {individual.age}.')
+            result.append(f'Error: US07: The individual {individual.name} ({individual.id}) is too old, age = {individual.age}.')
     return result
 
 def birth_before_death(gedcom):
@@ -20,7 +20,7 @@ def birth_before_death(gedcom):
         if death == None:
             continue
         elif death < birth:
-            result.append(f'Error: The individual {individual.name} ({individual.id}) has death before birth, death = {individual.death}, birth = {individual.birthday}')
+            result.append(f'Error: US03: The individual {individual.name} ({individual.id}) has death before birth, death = {individual.death}, birth = {individual.birthday}')
     
     return result
 
@@ -39,9 +39,9 @@ def birth_before_marriage(gedcom):
         if not mariage_date:
             continue
         if husband.birthday and husband.birthday > mariage_date:
-            result.append(f'Error: The individual {husband.name} ({husband.id}) has marriage before birth, marriage = {mariage_date}, birth = {husband.birthday}')
+            result.append(f'Error: US02: The individual {husband.name} ({husband.id}) has marriage before birth, marriage = {mariage_date}, birth = {husband.birthday}')
         if wife.birthday and wife.birthday > mariage_date:
-            result.append(f'Error: The individual {wife.name} ({wife.id}) has marriage before birth, marriage = {mariage_date}, birth = {wife.birthday}')
+            result.append(f'Error: US02: The individual {wife.name} ({wife.id}) has marriage before birth, marriage = {mariage_date}, birth = {wife.birthday}')
 
     return result
 
@@ -49,7 +49,7 @@ def validate_marriage_after_divorce(gedcom):
     result = []
     for family in gedcom.families:
         if (family.married is not None and family.divorced is not None) and (family.married > family.divorced):
-            result.append(f'Error: Family {family.id} has divorce date before marriage date')
+            result.append(f'Error: US04: Family {family.id} has divorce date {gedcom.date_string(family.divorced)} before marriage date {gedcom.date_string(family.married)}')
     return result
 
 def validate_dates_before_current(gedcom):
@@ -57,14 +57,14 @@ def validate_dates_before_current(gedcom):
     today = datetime.datetime.now()
     for family in gedcom.families:
         if family.married is not None and family.married > today:
-            result.append(f'Error: The Family {family.id}\'s marriage date is after the current date')
+            result.append(f'Error: US01: The Family {family.id}\'s marriage date {gedcom.date_string(family.married)} is after the current date')
         if family.divorced is not None and family.divorced > today:
-            result.append(f'Error: The Family {family.id}\'s divorce date is after the current date')
+            result.append(f'Error: US01: The Family {family.id}\'s divorce date {gedcom.date_string(family.divorced)} is after the current date')
     for individual in gedcom.individuals:
         if individual.birthday is not None and individual.birthday > today:
-            result.append(f'Error: The Individual {individual.id}\'s birthday is after the current date')
+            result.append(f'Error: US01: The Individual {individual.id}\'s birthday {gedcom.date_string(individual.birthday)} is after the current date')
         if individual.death is not None and individual.death > today:
-            result.append(f'Error: The Individual {individual.id}\'s deathdate is after the current date')  
+            result.append(f'Error: US01: The Individual {individual.id}\'s deathdate {gedcom.date_string(individual.death)} is after the current date')  
     
     return result
 
@@ -76,16 +76,16 @@ def validate_corresponding_entries(gedcom):
         if individual.child is not None:
             family = gedcom.family_with_id(individual.child)
             if family is None:
-                result.append(f'Error: The individual {individual.id} says they are a child of family {individual.child}, but that family has no record')
+                result.append(f'Error: US26: The individual {individual.id} says they are a child of family {individual.child}, but that family has no record')
             elif individual.id not in family.children:
-                result.append(f'Error: The individual {individual.id} says they are a child of family {family.id}, but that family does not list this individual as a child')
+                result.append(f'Error: US26: The individual {individual.id} says they are a child of family {family.id}, but that family does not list this individual as a child')
         
         if individual.spouse is not None:
             family = family = gedcom.family_with_id(individual.spouse)
             if family is None:
-                result.append(f'Error: The individual {individual.id} says they are a spouse of family {individual.spouse}, but that family has no record')
+                result.append(f'Error: US26: The individual {individual.id} says they are a spouse of family {individual.spouse}, but that family has no record')
             elif not family.husband_id == individual.id and not family.wife_id == individual.id:
-                result.append(f'Error: The individual {individual.id} says they are a spouse of family {family.id}, but that family does not list this individual as a spouse')
+                result.append(f'Error: US26: The individual {individual.id} says they are a spouse of family {family.id}, but that family does not list this individual as a spouse')
             
     # Make sure every family member's individual record also mentions the family
     for family in gedcom.families:
@@ -93,17 +93,17 @@ def validate_corresponding_entries(gedcom):
             if spouse_id is not None:
                 spouse = gedcom.individual_with_id(spouse_id)
                 if spouse is None:
-                    result.append(f'Error: The family {family.id} contains spouse {spouse_id}, but that individual has no record')
+                    result.append(f'Error: US26: The family {family.id} contains spouse {spouse_id}, but that individual has no record')
                 elif spouse.spouse != family.id:
-                    result.append(f'Error: The family {family.id} contains spouse {spouse_id}, but that individual does not list this family as a spouse')
+                    result.append(f'Error: US26: The family {family.id} contains spouse {spouse_id}, but that individual does not list this family as a spouse')
 
         if family.children is not None and len(family.children) > 0:
             for child_id in family.children:
                 child = gedcom.individual_with_id(child_id)
                 if child is None:
-                    result.append(f'Error: The family {family.id} contains child {child_id}, but that individual has no record')
+                    result.append(f'Error: US26: The family {family.id} contains child {child_id}, but that individual has no record')
                 elif child.child != family.id:
-                    result.append(f'Error: The family {family.id} contains child {child_id}, but that individual does not list this family as a child')
+                    result.append(f'Error: US26: The family {family.id} contains child {child_id}, but that individual does not list this family as a child')
     
     return result
 
@@ -116,9 +116,9 @@ def validate_marriage_after_fourteen(gedcom):
         
         if family.married is not None:
             if ((family.married - wife.birthday).days / 365) < 14:
-                errors.append(f'Error: Spouse {wife.id} in family {family.id} was married at less than 14 years old') 
+                errors.append(f'Error: US10: Spouse {wife.id} in family {family.id} was married at less than 14 years old, age={wife.age}') 
             if ((family.married - husband.birthday).days / 365) < 14:
-                errors.append(f'Error: Spouse {husband.id} in family {family.id} was married at less than 14 years old') 
+                errors.append(f'Error: US10: Spouse {husband.id} in family {family.id} was married at less than 14 years old, age={husband.age}') 
     
     return errors
 
@@ -127,7 +127,7 @@ def validate_fewer_than_15_sibs(gedcom):
     
     for family in gedcom.families:
         if len(family.children) > 15:
-            errors.append(f'Error: Family {family.id} has more than 15 siblings')
+            errors.append(f'Error: US15: Family {family.id} has more than 15 siblings')
     return errors
 
 def validate_marriage_before_death(gedcom):
@@ -140,14 +140,15 @@ def validate_marriage_before_death(gedcom):
         if marriage_date:
             husband = gedcom.individual_with_id(family.husband_id)
             wife = gedcom.individual_with_id(family.wife_id)
-            if husband.death < marriage_date:
-                result.append(f'Error: Family ({family.id}) has the husband {husband.name} ({family.husband_id})'
-                              f' death date ({husband.death.strftime("%x")}) before the marriage date ({marriage_date.strftime("%x")})')
-            if wife.death < marriage_date:
-                result.append(f'Error: Family ({family.id}) has the wife {wife.name} ({family.wife_id})'
-                              f' death date ({wife.death.strftime("%x")}) before the marriage date ({marriage_date.strftime("%x")})')
+            if husband.death is not None:
+                if husband.death < marriage_date:
+                    result.append(f'Error: US05: Family ({family.id}) has the husband {husband.name} ({family.husband_id})'
+                                f' death date ({husband.death.strftime("%x")}) before the marriage date ({marriage_date.strftime("%x")})')
+            if wife.death is not None:
+                if wife.death < marriage_date:
+                    result.append(f'Error: US05: Family ({family.id}) has the wife {wife.name} ({family.wife_id})'
+                                f' death date ({wife.death.strftime("%x")}) before the marriage date ({marriage_date.strftime("%x")})')
     return result
-
 
 def validate_divorce_before_death(gedcom):
     """
@@ -160,10 +161,10 @@ def validate_divorce_before_death(gedcom):
             husband = gedcom.individual_with_id(family.husband_id)
             wife = gedcom.individual_with_id(family.wife_id)
             if husband.death < divorce_date:
-                result.append(f'Error: Family ({family.id}) has the husband {husband.name} ({family.husband_id})'
+                result.append(f'Error: US05: Family ({family.id}) has the husband {husband.name} ({family.husband_id})'
                               f' death date ({husband.death.strftime("%x")}) before the divorce date ({divorce_date.strftime("%x")})')
             if wife.death < divorce_date:
-                result.append(f'Error: Family ({family.id}) has the wife {wife.name} ({family.wife_id})'
+                result.append(f'Error: US05: Family ({family.id}) has the wife {wife.name} ({family.wife_id})'
                               f' death date ({wife.death.strftime("%x")}) before the divorce date ({divorce_date.strftime("%x")})')
     return result
 
@@ -175,7 +176,9 @@ all_validators = [
     validate_too_old_individual, 
     validate_marriage_after_divorce, 
     birth_before_death, 
-    birth_before_marriage
+    birth_before_marriage,
+    validate_marriage_before_death,
+    validate_divorce_before_death
 ]
 
 
